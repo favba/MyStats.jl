d66e77740ce6a4b36ee8d6fcfc62c1ef2389a783
Felipe <felipe.avba@gmail.com>
2017-11-01 20:59:53 -0400

    Changed closed to :left

--- src/MyStats.jl
+++ src/MyStats.jl
@@ -6,7 +6,7 @@ export hist
 
 function hist(field::S,nbins::Int=250,pdf::Bool=true) where S<:AbstractArray
   array = @view(field[:])
-  h = fit(Histogram,array,nbins=nbins,closed=:right)
+  h = fit(Histogram,array,nbins=nbins,closed=:left)
   x = [(h.edges[1][i] + h.edges[1][i+1])/2 for i=1:(length(h.edges[1])-1)]
   if pdf
     y = h.weights/(sum(h.weights)*step(h.edges[1]))
@@ -18,7 +18,7 @@ end
 
 function hist(field::S, indices::Array{T,1}, nbins::Int=250,pdf::Bool=true) where {S<:AbstractArray,T<:Integer}
   array = @view(field[indices])
-  h = fit(Histogram,array,nbins=nbins,closed=:right)
+  h = fit(Histogram,array,nbins=nbins,closed=:left)
   x = [(h.edges[1][i] + h.edges[1][i+1])/2 for i=1:(length(h.edges[1])-1)]
   if pdf
     y = h.weights/(sum(h.weights)*step(h.edges[1]))
